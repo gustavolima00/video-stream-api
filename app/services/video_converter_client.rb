236a9075406bf9313abc91a57010565a5a1e33b3
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-20 17:15:16 -0300

    Addition of video converter client methods

--- app/services/video_converter_client.rb
+++ app/services/video_converter_client.rb
@@ -16,16 +16,78 @@ class VideoConverterClient
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
-
     file.close
 
-    response.is_a?(Net::HTTPSuccess)
+    unless response.is_a?(Net::HTTPSuccess)
+      raise "Error sending video to video converter - #{response.body}"
+    end
+
+    true
+  end
+
+  def self.get_video(uuid:)
+    uri = URI("#{VideoConverterClient.base_url}/raw-videos/#{uuid}")
+    request = Net::HTTP::Get.new(uri)
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    unless response.is_a?(Net::HTTPSuccess)
+      raise "Error getting video with uuid #{uuid} - #{response.body}"
+    end
+
+    JSON.parse(response.body)
+  end
+
+  def self.list_videos
+    uri = URI("#{VideoConverterClient.base_url}/raw-videos")
+    uri.query = URI.encode_www_form(userUuid: VideoConverterClient.webhook_user_uuid)
+
+    request = Net::HTTP::Get.new(uri)
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    unless response.is_a?(Net::HTTPSuccess)
+      raise "Error listing videos - #{response.body}"
+    end
+
+    JSON.parse(response.body)
+  end
+
+  def self.register_webhook_user
+    uri = URI("#{VideoConverterClient.base_url}/webhook-user/#{VideoConverterClient.webhook_user_uuid}")
+    uri.query = URI.encode_www_form(webhookUrl: VideoConverterClient.webhook_url, events: VideoConverterClient.webhook_events)
+
+    request = Net::HTTP::Put.new(uri)
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    unless response.is_a?(Net::HTTPSuccess)
+      raise "Error registering webhook user - #{response.body}"
+    end
+
+    JSON.parse(response.body)
   end
 
   private 
 
+  def self.webhook_events
+    %w[
+      VideoTracksExtracted
+      SubtitleTracksExtracted
+      VideoTrackExtractionFailed
+      SubtitleTrackExtractionFailed
+    ]
+  end
+
+  def self.webhook_url
+    ENV['VIDEO_CONVERTER_WEBHOOK_URL'] || 'http://localhost:3000'
+  end
+
   def self.base_url
-    ENV['VIDEO_CONVERTER_BASE_URL'] || 'http://localhost:5001'
+    ENV['VIDEO_CONVERTER_BASE_URL'] || 'http://localhost:5275'
   end
 
   def self.webhook_user_uuid
---
+++ config/initializers/video_converter_client.rb
@@ -0,0 +1,4 @@
+## Register webhook user
+
+require_relative '../../app/services/video_converter_client'
+VideoConverterClient.register_webhook_user
