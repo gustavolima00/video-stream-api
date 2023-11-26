826d08a6a14c1b1b53e8d40b3b44a0884db1dfbf
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-11-25 22:22:26 -0300

    Correction of title service calls

--- app/controllers/api/v1/titles.rb
+++ app/controllers/api/v1/titles.rb
@@ -7,14 +7,14 @@ module API
         resource :titles do
           desc "Return all users"
           get "" do
-            ListTitles.call()
+            TitleService.list
           end
         desc "Return a title"
           params do
             requires :id, type: String, desc: "ID of the title"
           end
           get ":id" do
-            GetTitle.call(title_id: params[:id])
+            TitleService.get(id: params[:id])
           end
         end
       end
